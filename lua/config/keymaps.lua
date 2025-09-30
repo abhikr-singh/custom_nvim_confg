03744ff9b26ddcbbef1d31cc7f61331033e70f5f
Abhishek Singh <abhishek.dumca@gmail.com>
2025-09-30 13:28:36 +0530

    added copilot plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<esc>", ":noh<CR><esc>", { desc = "Clear search highlights"
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazy" })
 vim.keymap.set("n", "<leader>qq", "<cmd>qa!<CR>", { desc = "Quit Neovim" })
+vim.g.copilot_no_tab_map = true
+vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
+vim.keymap.set("n", "<leader>cc", "<cmd>CopilotChatToggle<CR>", { desc = "Open CopilotChat" })
