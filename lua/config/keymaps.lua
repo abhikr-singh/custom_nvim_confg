aac23f948a0396b41203a94c81e3557a4e4b322c
Abhishek Singh <abhishek.dumca@gmail.com>
2025-09-30 13:14:45 +0530

    added snacks plugin and updated keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neotree" })
 vim.keymap.set("n", "<esc>", ":noh<CR><esc>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazy" })
+vim.keymap.set("n", "<leader>qq", "<cmd>qa!<CR>", { desc = "Quit Neovim" })
