fb1a2d0d06acaa48f500667d4d6c5197414ffe3a
Abhishek Singh <abhishek.dumca@gmail.com>
2025-10-01 10:11:08 +0530

    added neotest

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -6,6 +6,134 @@ return {
 			"nvim-neotest/nvim-nio",
 			"mfussenegger/nvim-dap-python",
 		},
+		keys = {
+			{
+				"<leader>td",
+				function()
+					require("neotest").run.run({ strategy = "dap" })
+				end,
+				desc = "Debug Nearest",
+			},
+			{
+				"<leader>dB",
+				function()
+					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+				end,
+				desc = "Breakpoint Condition",
+			},
+			{
+				"<leader>db",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "Toggle Breakpoint",
+			},
+			{
+				"<leader>dc",
+				function()
+					require("dap").continue()
+				end,
+				desc = "Run/Continue",
+			},
+			{
+				"<leader>da",
+				function()
+					require("dap").continue({ before = get_args })
+				end,
+				desc = "Run with Args",
+			},
+			{
+				"<leader>dC",
+				function()
+					require("dap").run_to_cursor()
+				end,
+				desc = "Run to Cursor",
+			},
+			{
+				"<leader>dg",
+				function()
+					require("dap").goto_()
+				end,
+				desc = "Go to Line (No Execute)",
+			},
+			{
+				"<leader>di",
+				function()
+					require("dap").step_into()
+				end,
+				desc = "Step Into",
+			},
+			{
+				"<leader>dj",
+				function()
+					require("dap").down()
+				end,
+				desc = "Down",
+			},
+			{
+				"<leader>dk",
+				function()
+					require("dap").up()
+				end,
+				desc = "Up",
+			},
+			{
+				"<leader>dl",
+				function()
+					require("dap").run_last()
+				end,
+				desc = "Run Last",
+			},
+			{
+				"<leader>do",
+				function()
+					require("dap").step_out()
+				end,
+				desc = "Step Out",
+			},
+			{
+				"<leader>dO",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "Step Over",
+			},
+			{
+				"<leader>dP",
+				function()
+					require("dap").pause()
+				end,
+				desc = "Pause",
+			},
+			{
+				"<leader>dr",
+				function()
+					require("dap").repl.toggle()
+				end,
+				desc = "Toggle REPL",
+			},
+			{
+				"<leader>ds",
+				function()
+					require("dap").session()
+				end,
+				desc = "Session",
+			},
+			{
+				"<leader>dt",
+				function()
+					require("dap").terminate()
+				end,
+				desc = "Terminate",
+			},
+			{
+				"<leader>dw",
+				function()
+					require("dap.ui.widgets").hover()
+				end,
+				desc = "Widgets",
+			},
+		},
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
@@ -38,27 +166,27 @@ return {
 			require("dap-python").setup(python_path)
 
 			-- Keymaps
-			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
-			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
-			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
-			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-			vim.keymap.set("n", "<leader>B", function()
-				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-			end, { desc = "Debug: Conditional Breakpoint" })
-			vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "Debug: Open REPL" })
-			vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Debug: Run Last" })
-
-			vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle DAP UI" })
-			vim.keymap.set("n", "<leader>dd", function()
-				dap.run({
-					type = "python",
-					request = "launch",
-					name = "Launch file",
-					program = vim.fn.expand("%:p"), -- current file
-					console = "externalTerminal",
-				})
-			end, { desc = "Debug: Run Current File" })
+			-- vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+			-- vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+			-- vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+			-- vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+			-- vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+			-- vim.keymap.set("n", "<leader>B", function()
+			-- 	dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			-- end, { desc = "Debug: Conditional Breakpoint" })
+			-- vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "Debug: Open REPL" })
+			-- vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Debug: Run Last" })
+			--
+			-- vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle DAP UI" })
+			-- vim.keymap.set("n", "<leader>dd", function()
+			-- 	dap.run({
+			-- 		type = "python",
+			-- 		request = "launch",
+			-- 		name = "Launch file",
+			-- 		program = vim.fn.expand("%:p"), -- current file
+			-- 		console = "externalTerminal",
+			-- 	})
+			-- end, { desc = "Debug: Run Current File" })
 		end,
 	},
 }
